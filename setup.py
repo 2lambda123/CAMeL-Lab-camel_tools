1f6ac2c265f3866c17fb5eceddff8a2dca32ba11
Restyled.io <commits@restyled.io>
2024-09-11 11:22:05 +0000

    Restyled by reorder-python-imports

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # MIT License
 #
 # Copyright 2018-2024 New York University Abu Dhabi
@@ -21,8 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-
 import os
 import sys
 
